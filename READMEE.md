a2ec2b0163c55eed5106caac53d0021bc316cf30
littlefire <aybwhite@gmail.com>
2020-11-06 23:57:10 -0800

    js code and bootstrap magic

--- READMEE.md
+++ READMEE.md
@@ -9,4 +9,56 @@
 
 ## BEFORE AND AFTER SCREENSHOTS
 
-![Starting Code.] (./assets/ScreenShot.img)
+![Starting Code.] (./assets/S)
+
+
+var save = document.querySelector('section');
+var savedText = document.querySelector('#input');
+var text = document.querySelector('#input');
+
+save.addEventListener('submit', function ( event ) {
+    //Don't save..
+    event.preventDefault();
+        //if empty
+        if (savedText.value.length < 1) return;
+
+    //Add item 
+    text.innerHTML;
+    //Clear ?
+    savedText.value= ' ';
+
+    //Save to localStorage
+    localStorage.setItem('savedText', text.innerHTML);
+
+console.log(localStorage);
+
+}, false);
+
+//Check for saved items and show saved
+var saveHistory = localStorage.getItem('savedText');
+
+    if (saveHistory) {
+        text.innerHTML = saved;
+    }
+
+//var myStorage = window.localStorage;
+//document.getElementById(".userTextArea").style.color = "grey";
+//onclick = document.getElementById
+
+
+ //   document.getElementById("p2").style.color = "blue";
+
+   // function changeBackground(color) {
+   //     document.body.style.background = color;
+   //  }
+     
+   //  window.addEventListener("load",function() { changeBackground('red') });
+
+   // $('.saveBtn').on('click', (e) => {
+    // LET hourClicked equal the buttons clicked sibling textarea's date-time.
+  //  let hourClicked = $(e.target).siblings('textarea').data('time')
+    // LET notesSaved equal the buttons clicked sibling textarea's value.
+ //   let notesSaved = $(e.target).siblings('textarea').val()
+    
+  //  localStorage.setItem('hour' + hourClicked, notesSaved)
+// });
---
+++ script.js
@@ -0,0 +1,54 @@
+var clearBtn = $('clearBtn');
+const currentDay = moment();
+
+//Shows current time in Header when page loads
+setInterval (function() {
+    var date = moment();
+    $(".date").text(date.format("[Today is:] MMM DD, YYYY [at] h:mm a"));
+    }, 1000);
+
+
+ // saveBtn event
+ $('.saveBtn').on('click',(event) => {
+    let saveButton = $(event.target).siblings('textarea').data('time')
+    let savedText = $(event.target).siblings('textarea').val()
+    
+    localStorage.setItem('hour' + saveButton, savedText)
+});
+
+// Accessing localStorage
+// Military time
+for (let hourIndex = 9; hourIndex <= 24; hourIndex++) {
+    let savedTodo = localStorage.getItem('hour' + hourIndex);
+    $(`#hour${hourIndex}`).text(savedTodo)
+        // If nothing is in the box then nothing will be saved= reset
+    if ($(`#hour${hourIndex}`).text() == null) {
+        $(`#hour${hourIndex}`).text().empty();
+    }
+    
+}
+
+//console.log(localStorage);//
+
+// Military time
+for (let timeIndex = 9; timeIndex <= 24; timeIndex++) {
+    const currentTime = $(`#hour${timeIndex}`).data('time');
+
+    if (parseInt(currentDay.format('H')) > currentTime) {
+        $(`#hour${timeIndex}`).addClass('past')
+    }
+    else if (parseInt(currentDay.format('H')) == currentTime) {
+        $(`#hour${timeIndex}`).addClass('present')
+    }
+    else if (parseInt(currentDay.format('H')) < currentTime) {
+        $(`#hour${timeIndex}`).addClass('future')
+    }
+}
+
+// Refreshing the planner!! WORK IN PROGRESS 
+$('clearBtn').on('click', (event) => {
+    event.preventDefault();
+        localStorage.empty();
+});
+
+console.log(clearBtn);
